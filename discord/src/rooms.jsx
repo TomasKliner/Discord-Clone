7b3f5622dba7d68d15b88d96b8b068b8fda676e5
Tomas Kliner <tomasklinerr@gmail.com>
2022-08-31 22:53:53 +0200

    changed server name formating

--- discord/src/rooms.jsx
+++ discord/src/rooms.jsx
@@ -3,17 +3,17 @@ import logo from "./logo.svg";
 export default function Rooms() {
   return (
     <div className="flex flex-col text-white bg-red-200 w-full">
-      <div className="w-full bg-neutral-500 text-white border-b-2 border-black">
+      <div className="w-full bg-neutral-500 text-white border-b-2 border-neutral-800 shadow">
         <div className="flex justify-between p-4 w-1/3 bg-neutral-600">
           <h2 className="font-bold">Server Name</h2>
-          <p>Arrow</p>
+          <p className=" font-black rotate-90"> > </p>
         </div>
       </div>
       <div className="flex bg-neutral-500 w-full h-full">
         <div className="flex flex-col w-1/3">
           <div className="bg-neutral-600 w-full h-full">Rooms</div>
           <div className="bg-neutral-700 w-full flex items-center justify-between p-2">
-            <div className="flex hover:bg-neutral-500 p-1 rounded-sm">
+            <div className="flex hover:bg-neutral-500 p-1 rounded-md">
               <img src={logo} alt="profile" className="w-10 cursor-pointer"></img>
               <div className="ml-2">
                 <h4 className="font-bold text-sm cursor-pointer">Tomáš Kliner</h4>
@@ -25,8 +25,8 @@ export default function Rooms() {
             <p className="hover:bg-neutral-500 p-1 rounded-sm cursor-pointer">Set</p>
           </div>
         </div>
-        <div className="w-1/3">s</div>
-        <div className="w-1/3 bg-neutral-600">s</div>
+        <div className="w-1/3">Chat</div>
+        <div className="w-1/3 bg-neutral-600">Names</div>
       </div>
     </div>
   );
