d873550c023ad163f4620f52d8e4372a5281ea33
Tomáš Kliner <klinetom@fit.cvut.cz>
2022-09-02 10:11:17 +0200

    added more rooms

--- discord/src/rooms.jsx
+++ discord/src/rooms.jsx
@@ -5,8 +5,24 @@ export default function Rooms(){
     <div className="flex flex-col justify-start">
         <div className="flex items-center bg-neutral-400 px-2 rounded mt-1 mx-4 pointer py-1"><div className="mr-2 font-light">#</div>Chat-Room</div>
         <div className="flex items-center hover:bg-neutral-400 px-2 rounded mt-1 mx-4 cursor-pointer py-1"><div className="mr-2 font-light">#</div>Chat-Room2</div>
-        
-        <div className="flex items-center hover:bg-neutral-400 px-2 rounded mt-1 mx-4 cursor-pointer py-1"><div className="mr-2 font-light">🔊</div>Talk</div>
+        <div className="my-2">
+        <div className="flex items-center hover:bg-neutral-400 px-2 rounded mt-1 mx-4 cursor-pointer py-1"><div className="mr-2 font-light">🔊</div>Talk1</div>
+        <div className="flex items-center hover:bg-neutral-400 px-2 rounded mt-1 mx-4 cursor-pointer py-1"><div className="mr-2 font-light">🔊</div>Talk2</div>
+        <div className="flex items-center hover:bg-neutral-400 px-2 rounded mt-1 mx-4 cursor-pointer py-1"><div className="mr-2 font-light">🔊</div>Talk3</div>
          </div>
+
+         <div className="my-2">
+             <div className="flex items-center text-sm relative hover:text-white"><div className="rotate-180 mx-2">^</div>Games<div className="absolute right-4 text-xl text-neutral-400 font-medium top-0">+</div></div>
+        <div className="flex items-center hover:bg-neutral-400 px-2 rounded mt-1 mx-4 cursor-pointer py-1"><div className="mr-2 font-light">🔊</div>Talk1</div>
+        <div className="flex items-center hover:bg-neutral-400 px-2 rounded mt-1 mx-4 cursor-pointer py-1"><div className="mr-2 font-light">🔊</div>Talk2</div>
+        <div className="flex items-center hover:bg-neutral-400 px-2 rounded mt-1 mx-4 cursor-pointer py-1"><div className="mr-2 font-light">🔊</div>Talk3</div>
+        </div>
+
+        <div className="my-2">
+        <div className="flex items-center hover:bg-neutral-400 px-2 rounded mt-1 mx-4 cursor-pointer py-1"><div className="mr-2 font-light">🔊</div>Talk1</div>
+        <div className="flex items-center hover:bg-neutral-400 px-2 rounded mt-1 mx-4 cursor-pointer py-1"><div className="mr-2 font-light">🔊</div>Talk2</div>
+        <div className="flex items-center hover:bg-neutral-400 px-2 rounded mt-1 mx-4 cursor-pointer py-1"><div className="mr-2 font-light">🔊</div>Talk3</div>
+         
+         </div></div>
     );
 }
