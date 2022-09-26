ace6d34c9f7e971467c12708e9eadeb54e88191f
Tomáš Kliner <klinetom@fit.cvut.cz>
2022-09-26 14:08:43 +0200

    Functioning input file modal

--- discord/src/chat.jsx
+++ discord/src/chat.jsx
@@ -33,8 +33,11 @@ export default function Chat() {
       <div className="w-full">
         {/* Input section*/}
         <div className="m-auto w-11/12 rounded-xl bg-neutral-600 mb-4 flex justify-between items-center">
-            <div className="bg-neutral-200 hover:bg-neutral-100 text-neutral-600 font-bold rounded-full aspect-square w-8 flex items-center justify-center text-xl ml-2 cursor-pointer">+</div>
-            <input type="test" className="bg-transparent outline-none w-full p-3" placeholder="Message # Chat-Room "  />
+            <label  className="bg-neutral-200 hover:bg-neutral-100 text-neutral-600 font-bold rounded-full aspect-square w-8 flex items-center justify-center text-xl ml-2 cursor-pointer">
+            <input id="file_input" type="file" className="hidden"/>
+              +
+              </label>
+            <input type="test" className="bg-transparent outline-none w-full p-3" placeholder="Message # Chat-Room "  title="+"/>
         </div>
       </div>
     </div>
