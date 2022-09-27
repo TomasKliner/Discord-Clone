0496a2499fc1b15a96168bea252805d1c03c504c
Tomáš Kliner <klinetom@fit.cvut.cz>
2022-09-27 16:32:54 +0200

    updated config with real Dicord colors

--- discord/tailwind.config.js
+++ discord/tailwind.config.js
@@ -7,7 +7,26 @@ module.exports = {
     "./public/*.{js,jsx,ts,tsx,svg}",
   ],
   theme: {
-    extend: {},
+    extend: {      
+ colors: {
+      transparent: 'transparent',
+      discord:{
+        white:'#4ffffff',
+        blue: '#7289da',
+        600:'#424549',
+        700:'#36393e',
+        800:'#282b30',
+        900:'#1e2124',
+      },
+      //comment this part out and do it propperly
+      neutral:{
+       600:'#424549',
+        700:'#36393e',
+        800:'#282b30',
+        900:'#1e2124',
+      }
+    },
+    },
   },
   plugins: [],
 }
